bdf2984d9e826181303554edf36bbbd511cf880f
Barbara-Mase <mase.barbara@gmail.com>
2026-07-01 18:10:10 +0200

    Css en cours : menu burger

--- src/api/cover_img_function.js
+++
@@ -1,7 +0,0 @@
-export function fetchCover(coverId, img) {
-    const url = `https://covers.openlibrary.org/b/id/${coverId}-M.jpg`;
-
-    fetch(url)
-        .then(response => response.blob())
-        .then(blob => { img.src = URL.createObjectURL(blob); })
-        .catch(error => console.error("Erreur image :", error));
